6336e9de4b59c0d74a10c9a194a47422d2c4bdc2
numoonchld <raghavendra.saralaya@gmail.com>
2021-02-11 16:43:12 +0530

    image bezier scaling added

--- crown-clothing/src/components/menu-item/MenuItem.js
+++ crown-clothing/src/components/menu-item/MenuItem.js
@@ -1,15 +1,16 @@
 import React, { Fragment } from 'react'
 import './MenuItem.scss'
 
-const MenuItem = ({title, imageUrl}) => {
+const MenuItem = ({ title, imageUrl, size }) => {
     console.log(imageUrl)
     return <Fragment>
-        <div style={{ backgroundImage: `url(${imageUrl})` }} className="menu-item">
-
-            <div  className='content'>
+        <div  className={`menu-item ${size}`}>
+            <div className='background-image' style={{ backgroundImage: `url(${imageUrl})` }} />
+            <div className='content'>
                 <h1 className='title'>{title.toUpperCase()}</h1>
                 <span className='subtitle'>SHOP NOW</span>
             </div>
+
 
         </div>
     </Fragment>
