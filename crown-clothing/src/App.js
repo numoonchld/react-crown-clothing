e59fcc0490dfb59c3b24903ffbb07e92be3d921a
numoonchld <raghavendra.saralaya@gmail.com>
2021-02-14 18:34:01 +0530

    routing setup using withRouter

--- crown-clothing/src/App.js
+++ crown-clothing/src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import HomePage from './pages/homepage/HomePage'
 import './App.css';
+import { Route, Switch } from 'react-router-dom'
+
+
+const HatsPage = () => <div>
+  <h1>HATS PAGE</h1>
+</div>
+
 
 function App() {
   return (
+
     <div className="App">
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
+
     </div>
+
   );
 }
 
--- crown-clothing/src/components/menu-item/MenuItem.js
+++ crown-clothing/src/components/menu-item/MenuItem.js
@@ -1,19 +1,18 @@
 import React, { Fragment } from 'react'
 import './MenuItem.scss'
+import { withRouter } from 'react-router-dom'
 
-const MenuItem = ({ title, imageUrl, size }) => {
-    console.log(imageUrl)
+const MenuItem = ({ title, imageUrl, linkUrl, size, history, location, match }) => {
+    // console.log("Current URL: ", match.url)
     return <Fragment>
-        <div  className={`menu-item ${size}`}>
+        <div  className={`menu-item ${size}`} onClick={()=>history.push(`${match.url}${linkUrl}`)}>
             <div className='background-image' style={{ backgroundImage: `url(${imageUrl})` }} />
             <div className='content'>
                 <h1 className='title'>{title.toUpperCase()}</h1>
                 <span className='subtitle'>SHOP NOW</span>
             </div>
-
-
         </div>
     </Fragment>
 }
 
-export default MenuItem
+export default withRouter(MenuItem)
