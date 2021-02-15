d0b037c71e195c67afae8acea25e59d5d44e1bfd
numoonchld <raghavendra.saralaya@gmail.com>
2021-02-15 20:55:26 +0530

    hedaer component added

--- crown-clothing/src/App.js
+++ crown-clothing/src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import HomePage from './pages/homepage/HomePage'
-import './App.css';
+import './App.scss';
 import { Route, Switch } from 'react-router-dom'
 import Shop from './pages/shop/Shop'
+import Header from './components/header/Header'
 
 
 function App() {
   return (
 
     <div className="App">
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path="/shop" component={Shop} />
