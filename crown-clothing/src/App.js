f1ba909978da40846190c847abf08cc24b935001
numoonchld <raghavendra.saralaya@gmail.com>
2021-02-16 11:15:54 +0530

    custom button in account page

--- crown-clothing/src/App.js
+++ crown-clothing/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import HomePage from './pages/homepage/HomePage'
+import Account from './pages/account/Account'
 import './App.scss';
 import { Route, Switch } from 'react-router-dom'
 import Shop from './pages/shop/Shop'
 import Header from './components/header/Header'
+
 
 
 function App() {
@@ -14,6 +16,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path="/shop" component={Shop} />
+        <Route path='/account' component={Account} />
       </Switch>
 
     </div>
---
+++ crown-clothing/src/pages/account/Account.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react'
+import SignIn from '../../components/sign-in/SignIn'
+import './Account.scss'
+
+const Account = () => {
+    return <Fragment>
+        <div> <SignIn /> </div>
+    </Fragment>
+}
+
+export default Account
